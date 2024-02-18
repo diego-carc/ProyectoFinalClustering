d315e69080fe78c669573100e42a5fdff24a3af2
ethan-gr <ethangr@lcg.unam.mx>
2024-02-18 00:19:57 -0600

    update

--- R/hclustTree.R
+++ R/hclustTree.R
@@ -1,22 +1,26 @@
+library(cluster)
 library(ape)
+
 disim.mtrx <- read.table("results/disimMatrix.tsv")
 
 # Save dendograms as phylogeny tree
 
 # complete linkage
-write.tree(
-  as.phylo(hclust(dist(disim.mtrx), method="complete")),
-  file = "results/phyloTree_complete.newick"
-)
+cl <- hclust(dist(disim.mtrx), method="complete")
+write.tree(as.phylo(cl),file="results/phyloTree_complete.newick")
+coef(cl)
 
 # single linkage
-write.tree(
-  as.phylo(hclust(dist(disim.mtrx), method="single")),
-  file = "results/phyloTree_single.newick"
-)
+sl <- hclust(dist(disim.mtrx), method="single")
+write.tree(as.phylo(sl), file="results/phyloTree_single.newick")
+coef(sl)
 
 # average linkage
-write.tree(
-  as.phylo(hclust(dist(disim.mtrx), method="average")),
-  file = "results/phyloTree_single.newick"
-)
+al <- hclust(dist(disim.mtrx), method="average")
+write.tree(as.phylo(al), file="results/phyloTree_average.newick")
+coef(al)
+
+# ward
+wd <- hclust(dist(disim.mtrx), method="ward")
+write.tree(as.phylo(wd), file="results/phyloTree_ward.newick")
+coef(wd)
